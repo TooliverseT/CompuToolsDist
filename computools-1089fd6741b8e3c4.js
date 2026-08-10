cfd2a6d817aa06c9331d71eaf1b5d198ca25ee4b
TooliverseT <TooliverseT@users.noreply.github.com>
2026-08-10 10:44:38 +0000

    deploy: TooliverseT/CompuTools@f6e3d4a9ee3610f1308f7ef4363ad8a155ce8f65

--- computools-1089fd6741b8e3c4.js
+++ computools-1089fd6741b8e3c4.js
@@ -264,8 +264,20 @@ if (!('encodeInto' in cachedTextEncoder)) {
 
 let WASM_VECTOR_LEN = 0;
 
+function wasm_bindgen_98644e2abeb9822___convert__closures________invoke___web_sys_7771eb89ff97b277___features__gen_Event__Event______1_(arg0, arg1, arg2) {
+    wasm.wasm_bindgen_98644e2abeb9822___convert__closures________invoke___web_sys_7771eb89ff97b277___features__gen_Event__Event______1_(arg0, arg1, arg2);
+}
+
+function wasm_bindgen_98644e2abeb9822___convert__closures_____invoke___web_sys_7771eb89ff97b277___features__gen_TouchEvent__TouchEvent_____(arg0, arg1, arg2) {
+    wasm.wasm_bindgen_98644e2abeb9822___convert__closures_____invoke___web_sys_7771eb89ff97b277___features__gen_TouchEvent__TouchEvent_____(arg0, arg1, arg2);
+}
+
 function wasm_bindgen_98644e2abeb9822___convert__closures_____invoke___web_sys_7771eb89ff97b277___features__gen_MouseEvent__MouseEvent_____(arg0, arg1, arg2) {
     wasm.wasm_bindgen_98644e2abeb9822___convert__closures_____invoke___web_sys_7771eb89ff97b277___features__gen_MouseEvent__MouseEvent_____(arg0, arg1, arg2);
+}
+
+function wasm_bindgen_98644e2abeb9822___convert__closures________invoke___web_sys_7771eb89ff97b277___features__gen_Event__Event______2_(arg0, arg1, arg2) {
+    wasm.wasm_bindgen_98644e2abeb9822___convert__closures________invoke___web_sys_7771eb89ff97b277___features__gen_Event__Event______2_(arg0, arg1, arg2);
 }
 
 function wasm_bindgen_98644e2abeb9822___convert__closures_____invoke___wasm_bindgen_98644e2abeb9822___JsValue_____(arg0, arg1, arg2) {
@@ -276,24 +288,12 @@ function wasm_bindgen_98644e2abeb9822___convert__closures_____invoke_______1_(ar
     wasm.wasm_bindgen_98644e2abeb9822___convert__closures_____invoke_______1_(arg0, arg1);
 }
 
-function wasm_bindgen_98644e2abeb9822___convert__closures_____invoke___web_sys_7771eb89ff97b277___features__gen_TouchEvent__TouchEvent_____(arg0, arg1, arg2) {
-    wasm.wasm_bindgen_98644e2abeb9822___convert__closures_____invoke___web_sys_7771eb89ff97b277___features__gen_TouchEvent__TouchEvent_____(arg0, arg1, arg2);
-}
-
-function wasm_bindgen_98644e2abeb9822___convert__closures_____invoke______(arg0, arg1) {
-    wasm.wasm_bindgen_98644e2abeb9822___convert__closures_____invoke______(arg0, arg1);
-}
-
 function wasm_bindgen_98644e2abeb9822___convert__closures________invoke___web_sys_7771eb89ff97b277___features__gen_Event__Event_____(arg0, arg1, arg2) {
     wasm.wasm_bindgen_98644e2abeb9822___convert__closures________invoke___web_sys_7771eb89ff97b277___features__gen_Event__Event_____(arg0, arg1, arg2);
 }
 
-function wasm_bindgen_98644e2abeb9822___convert__closures________invoke___web_sys_7771eb89ff97b277___features__gen_Event__Event______2_(arg0, arg1, arg2) {
-    wasm.wasm_bindgen_98644e2abeb9822___convert__closures________invoke___web_sys_7771eb89ff97b277___features__gen_Event__Event______2_(arg0, arg1, arg2);
-}
-
-function wasm_bindgen_98644e2abeb9822___convert__closures________invoke___web_sys_7771eb89ff97b277___features__gen_Event__Event______1_(arg0, arg1, arg2) {
-    wasm.wasm_bindgen_98644e2abeb9822___convert__closures________invoke___web_sys_7771eb89ff97b277___features__gen_Event__Event______1_(arg0, arg1, arg2);
+function wasm_bindgen_98644e2abeb9822___convert__closures_____invoke______(arg0, arg1) {
+    wasm.wasm_bindgen_98644e2abeb9822___convert__closures_____invoke______(arg0, arg1);
 }
 
 function wasm_bindgen_98644e2abeb9822___convert__closures_____invoke___wasm_bindgen_98644e2abeb9822___JsValue__wasm_bindgen_98644e2abeb9822___JsValue_____(arg0, arg1, arg2, arg3) {
